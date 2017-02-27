d57140904c7fd970d17a3ebd5c33d57adc8c3fe4
luojurong <617091106@qq.com>
2017-02-27 11:30:19 +0800

    hotadapter

--- app/src/main/java/com/animation/animation/fragment/HotFragment.java
+++ app/src/main/java/com/animation/animation/fragment/HotFragment.java
@@ -10,11 +10,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.animation.animation.R;
+import com.animation.animation.adapter.HotRecycleViewAdapter;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by luojurong on 2017/2/18.
@@ -23,18 +27,32 @@ import org.xutils.x;
 @ContentView(R.layout.hot_fragment_layout)
 public class HotFragment extends Fragment{
 
-    @ViewInject(R.id.rView_hotFragment)
+    List<String> data = new ArrayList<String>();
+
+  //  @ViewInject(R.id.rView_hotFragment)
     RecyclerView recyclerView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        RecyclerView recyclerView = new RecyclerView(getActivity());
+     //   RecyclerView recyclerView = new RecyclerView(getActivity());
+        View view = inflater.inflate(R.layout.hot_fragment_layout, container,false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.rView_hotFragment);
+        setAdapter(inflater);
         return x.view().inject(this,inflater,container);
     }
 
-//    @Event(type = View.OnClickListener.class, value = R.id.img_fragment)
+    private void setAdapter(LayoutInflater inflater) {
+        for (int i = 0; i <10; i++) {
+            data.add("lalala"+i);
+        }
+        HotRecycleViewAdapter adapter = new HotRecycleViewAdapter(getActivity(),data,inflater);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    //    @Event(type = View.OnClickListener.class, value = R.id.img_fragment)
     private void onclick(View view){
 
         ((ImageView)view).setImageResource(android.R.drawable.ic_menu_add);
